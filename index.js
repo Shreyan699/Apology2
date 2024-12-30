8e51ca5958891142a0cd9d1bce215ab3b10233c9
Shreyan699 <shreyanbiswas128@gmail.com>
2024-12-30 19:28:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+function proceed() {
+    let inputName = document.getElementById('name').value
+
+    if (inputName == "Subhashree") {
+        alert("Welcome")
+        window.location.href = "https://shreyan699.github.io/Apology2/page.html";
+    }
+    
+    else if (inputName == "Khushi") {
+        alert("Aap try kyu kr rhe ho 😡😡")
+    }
+    else if (inputName == "Yashika") {
+        alert("jao katti")
+    }
+    else if (inputName == "Anchal") {
+        alert("Aapke liye likha hh ye na hi inke liye")
+       
+    } 
+    else if (inputName == "Shelly") {
+        alert("Bye")
+    }
+    else{
+        alert("Access Prohibited")
+    }
+
+
+
+
+}
+
